394784c8cf17ac9cab06ce361887984717960c6d
Dmitry Nekrasov <scannorone@gmail.com>
2025-02-15 17:35:29 +0400

    Add depth field to TreeNode

--- src/main/kotlin/kotlinx/io/ropebytestring/TreeNode.kt
+++ src/main/kotlin/kotlinx/io/ropebytestring/TreeNode.kt
@@ -1,17 +1,22 @@
 package kotlinx.io.ropebytestring
 
+import kotlin.math.max
+
 internal sealed class TreeNode {
     abstract val weight: Int
+    abstract val depth: Int
 
     class Branch(
         val left: TreeNode,
         val right: TreeNode,
-        override val weight: Int = left.weight + right.weight
+        override val weight: Int = left.weight + right.weight,
+        override val depth: Int = max(left.depth, right.depth) + 1
     ) : TreeNode()
 
     class Leaf(
         val data: ByteArray,
-        override val weight: Int = data.size
+        override val weight: Int = data.size,
+        override val depth: Int = 1
     ) : TreeNode()
 
     companion object {
