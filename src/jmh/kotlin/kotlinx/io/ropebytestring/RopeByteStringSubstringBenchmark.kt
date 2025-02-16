39c80076915ebe81641511c22c2a9e3faa2f25d2
Dmitry Nekrasov <scannorone@gmail.com>
2025-02-16 18:27:43 +0400

    Add RopeByteStringSubstringBenchmark

---
+++ src/jmh/kotlin/kotlinx/io/ropebytestring/RopeByteStringSubstringBenchmark.kt
@@ -0,0 +1,43 @@
+package kotlinx.io.ropebytestring
+
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import java.util.concurrent.TimeUnit
+import kotlin.random.Random
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Fork(1)
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 2, time = 1)
+open class RopeByteStringSubstringBenchmark {
+    @Param("100", "10000")
+    private var stringSize: Int = 0
+
+    @Param("10", "5000")
+    private var chunkSize: Int = 0
+
+    @Param("10", "75")
+    private var windowPercentage: Int = 0
+
+    private lateinit var rope: RopeByteString
+    private var windowSize: Int = 0
+
+    @Setup
+    fun setup() {
+        rope = RopeByteString(
+            data = ByteArray(stringSize) { Random.nextBytes(1).first() },
+            chunkSize = chunkSize
+        )
+        windowSize = stringSize * windowPercentage / 100
+    }
+
+    @Benchmark
+    fun slidingWindow(blackhole: Blackhole) {
+        for (i in 0..<(rope.size - windowSize)) {
+            val sub = rope.substring(i, i + windowSize)
+            blackhole.consume(sub)
+        }
+    }
+}
